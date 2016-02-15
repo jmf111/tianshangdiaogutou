30edd76ce807b0e24427f05cb9fb9f5430e46b07
jmf111 <1169880473@qq.com>
2016-02-15 20:23:48 +0800

    element-2new

--- js/jiegutou.js
+++ js/jiegutou.js
@@ -2,10 +2,12 @@ window.onresize = function() {
 	location.reload()
     }
 
-		var x=0;;
-		var y=1;
-		var w=document.body.clientWidth;
-		var h=document.body.clientHeight;
+		var x=0;//鼠标的横坐标
+		var y=0;//接到的个数
+		var mx=0;//鼠标横坐标
+		var sum=0;//错过的个数
+		var w=document.body.clientWidth;//获得屏幕宽度
+		var h=document.body.clientHeight;//获得屏幕高度
 		var DownSpeed=new Array();
 		var Downsize=new Array();
 		var Bonex=new Array();
@@ -21,36 +23,43 @@ window.onresize = function() {
 		bones[8]=document.getElementById("bone8");
 		bones[9]=document.getElementById("bone9");
 		function move(){
+			event.preventDefault();
 			x=event.clientX;
 			document.getElementById("dog").style.backgroundPositionX=(x-30)+"px";
 			
 		}//接收器对象
+		if(event){
+			mx=event.clientX;
+		}
 	    for(i=0;i<9;i++){
-	         Bonex[i]= Math.random()*w*0.9;
+	         Bonex[i]= Math.random()*w*0.9;//产生一个随机下落的横坐标
 	         Downsize[i]=Math.random()*5+1;
-	         DownSpeed[i]=Math.random();
+	         DownSpeed[i]=Math.random();//产生一个随机的下落速度
 	        }
 	    function down(){
-	    	for(i=0;i<9;i++){
+	    	for(i=0;i<9;i++){//开始下落
 	    	    bones[i].style.left=Bonex[i]+"px";
 	    		DownSpeed[i]+=Downsize[i];
 	    		bones[i].style.top=DownSpeed[i]+"px";
 	    		dh=DownSpeed[i];
-	    		if(dh>h*0.9+30){
+	    		if(dh>h*0.9+30){//判断是否到底部并销毁产生新的坠落物
 	    			DownSpeed[i]=0;
 	    			Bonex[i]= Math.random()*w*0.9;
 	                Downsize[i]=Math.random()*5+1;
 	                DownSpeed[i]=Math.random();
+	                sum++;
 	    		}
-	    		if(bones[i].style.top>h*0.8&&bones[i].style.left==pageXOffset){
+	    		if(bones[i].style.top>h*0.8&&(Math.floor(bones[i].style.left)==mx)){//判断是否与接收器相碰，并销毁产生新的坠落物
 	    			DownSpeed[i]=0;
 	    			Bonex[i]= Math.random()*w*0.9;
 	                Downsize[i]=Math.random()*5+1;
 	                DownSpeed[i]=Math.random();
 	                y++;
-	                document.getElementById("score").innerHTML="得分"+y;
+	                sum++;
+	                document.getElementById("score").innerHTML="得分"+y+"分";
 	    				   
 	    		}
+	    		document.getElementById("sum").innerHTML="共错过"+sum+"个";
 	    	}
 	    	setTimeout("down()", 100);
 	    }
